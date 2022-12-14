f378396f0fcb7bb3817526693c683f91dbd06970
yanglinshu <nicolasvandarkholme@outlook.com>
2022-12-15 01:44:20 +0800

    For deploy only

---
+++ src/config/private.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJh7qJP_po7a_PCz5NxRapTDbSdBmXWoo",
+    authDomain: "folst2021.firebaseapp.com",
+    projectId: "folst2021",
+    storageBucket: "folst2021.appspot.com",
+    messagingSenderId: "1036254956622",
+    appId: "1:1036254956622:web:7f3f775df8a189c8e0c71f"
+};
+
+export {
+    firebaseConfig
+}
